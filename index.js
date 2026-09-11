38000fb30b37dc93d1792cd7ad93bc4c441e0f6e
Phillip Whipple <illwhip1982@gmail.com>
2026-09-11 19:23:22 -0400

    fixed: java script

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function openMenu() {
-  document.body.classlist += " menu--open"
+  document.body.classList += " menu--open"
 }
 function closeMenu() {
-  document.body.classlist.remove('menu--open')
+  document.body.classList.remove('menu--open')
 }
-
-console.log('time to cry')
